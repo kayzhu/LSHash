d39b16ed8f24655d80213ab78d9230267960b179
Kay Zhu <me@kayzhu.com>
2012-12-25 18:08:36 -0800

    added contributors list

---
+++ LSHash/__init__.py
@@ -0,0 +1,5 @@
+# LSHash/__init__.py
+# Copyright 2012 Kay Zhu (a.k.a He Zhu) and contributors (see CONTRIBUTORS.txt)
+#
+# This module is part of LSHash and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
